b38144927af6b06a65bdb254afa24df8db18d446
2015440108 <tpgml429@naver.com>
2018-11-06 17:39:32 +0900

    class

---
+++ 20181106_swap/swap.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void swap(int* ptr1, int* ptr2){//swap: 변수 a,b의 값을 바꾼다
+	int temp;
+	temp=*ptr1;
+	*ptr1=*ptr2;
+	*ptr2=temp;
+}
+
+int main(){
+	int a=10, b=20;
+	swap(&a,&b);
+
+	printf("a=%d, b=%d\n",a,b);
+
+	return 0;
+}
